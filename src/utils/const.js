9319ed8ca9db1310ecbafc81a3a56c000fcf3220
zhangshiqian <zhangshiqian.2020@bytedance.com>
2020-10-31 21:54:35 +0800

    config

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,5 @@
 // const
-const MAPBOX_TOKEN = 'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoic3F6aGFuZyIsImEiOiJja2d4Zml0YmUwYjZqMnJvc3ZocjF0YmxpIn0.3R7_tYTQGE4zVD6o0Buh2g';
 const MUNICIPALITY_CITIES_ARR = ['北京市', '上海市', '天津市', '重庆市', '香港特别行政区', '澳门特别行政区'];
 const IS_CHINESE = true
 
